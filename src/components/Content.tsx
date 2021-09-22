3fc1bb3b9c44cb6b851d02e33aafcec45d5e2d14
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-09-22 13:20:46 -0300

    Update

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,8 +1,5 @@
 export function Content() {
-  
-  
-  
-  return (
+  return(
     <div className="container">
       <header>
         <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
@@ -11,12 +8,11 @@ export function Content() {
       <main>
         <div className="movies-list">
           {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
           ))}
         </div>
       </main>
     </div>
-    </div >
   )
 
 
